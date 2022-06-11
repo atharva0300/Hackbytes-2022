0f5aa759a4d7245ed57b4703ebcedb73aca048ef
= <=>
2022-06-11 23:37:06 +0530

    photography folder

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     <nav className='w-screen h-24 bg-black flex flex-row justify-between'>
         <h2 className='self-center text-4xl text-white font-bold ml-24'>Hobbies</h2>
         <div className='self-center flex flex-row justiy-around mr-24'>
-            <a href = "#" className='self-center text-xl text-white font-normal px-6'>Sports</a>
+            <a href = "#" className='self-center text-xl text-white font-normal px-6 w-32 h-16 bg-fuchsia-500'>Sports</a>
             <a href = "#" className='self-center text-xl text-white font-normal px-6'>Music</a>
             <a href = "#" className='self-center text-xl text-white font-normal px-6'>Photography</a>
             <a href = "#" className='self-center text-xl text-white font-normal px-6'>About us</a>
